d7b39af688019251c715d4b280f780aefa2ea500
Yummy Bian <yummy.bian@gmail.com>
2018-02-28 18:25:25 +0800

    Add sudo privilege

--- travis_build_darknet.sh
+++ travis_build_darknet.sh
@@ -6,6 +6,6 @@ DARKNET="darknet"
 git clone https://github.com/ZanLabs/darknet.git 
 cd ${DARKNET}
 # optionally GPU=1
-make OPENCV=1 && make install
+sudo make OPENCV=1 && sudo make install
 cd ..
 rm -rf ${DARKNET} 
