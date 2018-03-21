7802a94383dbd82eda61e1d0754149b3de08bc36
Yummy Bian <yummy.bian@gmail.com>
2018-03-21 21:26:55 +0800

    Update travis

--- travis_build_cuda.sh
+++ travis_build_cuda.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# install CUDA Toolkit v8.0
-# instructions from https://developer.nvidia.com/cuda-downloads (linux -> x86_64 -> Ubuntu -> 16.04 -> deb (network))
-CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
+
+# install CUDA Toolkit v9.1
+CUDA_REPO_PKG="cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64"
+wget https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/${CUDA_REPO_PKG}
 sudo dpkg -i ${CUDA_REPO_PKG}
+sudo apt-key add /var/cuda-repo-9-1-local/7fa2af80.pub
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get install --force-yes -y cuda 
 
 # install cuDNN v6.0
 CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
 wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
 tar -xzvf ${CUDNN_TAR_FILE}
-sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
-sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
-sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda/include
+sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda/lib64/
+sudo chmod a+r /usr/local/cuda/lib64/libcudnn*
 
 # set environment variables
-export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
